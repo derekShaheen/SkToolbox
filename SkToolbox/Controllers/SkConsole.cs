0b13a46c48c298fb1d37df177c07f6688af29d63
Derek Shaheen <38998433+derekShaheen@users.noreply.github.com>
2022-04-04 00:27:01 -0500

    Removed UI reference

--- SkToolbox/Controllers/SkConsole.cs
+++ SkToolbox/Controllers/SkConsole.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.UI;
 
 /// <summary>
 /// A console to display Unity's debug logs in-game.
