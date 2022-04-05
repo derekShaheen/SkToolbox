650bbb3d40d04d2d267f9d5bc240435ea256b0bb
Derek Shaheen <38998433+derekShaheen@users.noreply.github.com>
2022-04-05 00:44:42 -0500

    Harmony included. SkPatcher added.

---
+++ SkToolbox/Controllers/SkPatcher.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+using SkToolbox.Utility;
+using System;
+
+namespace SkToolbox.Controllers
+{
+    public static class SkPatcher
+    {
+        private static Harmony harmony = null;
+        private static bool initComplete = false;
+
+        public static bool InitComplete { get => initComplete; set => initComplete = value; }
+
+        public static void InitPatch()
+        {
+
+            if (!InitComplete)
+            {
+                //SkUtilities.Logz(new string[] { "SkCommandPatcher", "INJECT" }, new string[] { "Attempting injection..." });
+                try
+                {
+                    //Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+                    harmony = Harmony.CreateAndPatchAll(typeof(SkPatcher).Assembly);
+                    //SkUtilities.Logz(new string[] { "SkCommandPatcher", "INJECT" }, new string[] { "INJECT => COMPLETE" });
+                }
+                catch (Exception ex)
+                //catch (Exception)
+                {
+                    SkUtilities.Logz(new string[] { "SkCommandPatcher", "PATCH" }, new string[] { "PATCH => FAILED.", ex.Message, ex.StackTrace }, UnityEngine.LogType.Error);
+                }
+                finally
+                {
+                    InitComplete = true;
+                }
+            }
+        }
+    }
+}
