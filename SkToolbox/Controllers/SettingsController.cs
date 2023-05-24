a6258ef4d0548030f7d50d9c3f670bd4a99fdda5
Derek Shaheen <38998433+derekShaheen@users.noreply.github.com>
2023-05-23 23:23:46 -0500

    Refactored the default config

--- SkToolbox/Controllers/SettingsController.cs
+++ SkToolbox/Controllers/SettingsController.cs
@@ -21,25 +21,27 @@ namespace SkToolbox.Controllers
         public static void Init(ConfigFile config)
         {
             _configFile = config;
-            s_config.Add("- Index", config.Bind("- Index", "ThisIsJustAnIndex-NotASetting", true, "Config sections:" +
-                                                        "\n" + CategoryGeneral +
-                                                        "\n" + CategoryConsoleDraw +
-                                                        "\n" + CategoryOther +
-                                                        // ...
-                                                        "\n"));
+            s_config = new Dictionary<string, ConfigEntryBase>
+            {
+                {"- Index", config.Bind("- Index", "ThisIsJustAnIndex-NotASetting", true, "Config sections:" +
+                                                                    "\n" + CategoryGeneral +
+                                                                    "\n" + CategoryConsoleDraw +
+                                                                    "\n" + CategoryOther +
+                                                                    // ...
+                                                                    "\n")},
+                {"DisplayToggleKey",    config.Bind(CategoryGeneral, "DisplayToggleKey", "BackQuote", "Key that toggles the on-screen display. BackQuote is also the tilde (` / ~) button.")},
+                {"ConsoleEnabled",      config.Bind(CategoryGeneral, "ConsoleEnabled", true, "Whether the console should be enabled or not.")},
+                {"PanelEnabled",        config.Bind(CategoryGeneral, "PanelEnabled", true, "Whether the panel should be enabled or not.")},
+                {"NetworkFunctions",    config.Bind(CategoryGeneral, "NetworkFunctions", true, "Determines if networking functions are enabled.")},
+                {"ConsoleTheme",        config.Bind(CategoryConsoleDraw, "ConsoleTheme", Color.grey, "Sets the overall color theme. Accepts hex and some standard color words.")},
+                {"ConsolePosition",     config.Bind(CategoryConsoleDraw, "ConsolePosition", Settings.Console.ConsolePos.TopCentered)},
+                {"ConsoleWidth",        config.Bind(CategoryConsoleDraw, "ConsoleWidth", -1, "Positive in pixels. Negative numbers will divide the screen by the number given. Ex. -2 will divide the screen in half, -3 in thirds, etc..")},
+                {"ConsoleHeight",       config.Bind(CategoryConsoleDraw, "ConsoleHeight", -2, "Positive in pixels. Negative numbers will divide the screen by the number given. Ex. -2 will divide the screen in half, -3 in thirds, etc..")},
+                {"ConsoleMaxOutput",    config.Bind(CategoryConsoleDraw, "ConsoleMaxOutput", 999, "Maximum number of lines to display in output window.")},
+                {"ConsoleFontSize",     config.Bind(CategoryConsoleDraw, "ConsoleFontSize", 16, "Size of the font within the console window.")},
+                {"ConsoleDarkenBackground", config.Bind(CategoryConsoleDraw, "ConsoleDarkenBackground", false, "Darken the rest of the screen around the console when visible.")}
+            };
 
-            s_config.Add("DisplayToggleKey", config.Bind(CategoryGeneral, "DisplayToggleKey", "BackQuote", "Key that toggles the on-screen display. BackQuote is also the tilde (` / ~) button."));
-            s_config.Add("ConsoleEnabled", config.Bind(CategoryGeneral, "ConsoleEnabled", true, "Whether the console should be enabled or not."));
-            s_config.Add("PanelEnabled", config.Bind(CategoryGeneral, "PanelEnabled", true, "Whether the panel should be enabled or not."));
-            s_config.Add("NetworkFunctions", config.Bind(CategoryGeneral, "NetworkFunctions", true, "Determines if networking functions are enabled."));
-
-            s_config.Add("ConsoleTheme", config.Bind(CategoryConsoleDraw, "ConsoleTheme", Color.grey, "Sets the overall color theme. Accepts hex and some standard color words."));
-            s_config.Add("ConsolePosition", config.Bind(CategoryConsoleDraw, "ConsolePosition", Settings.Console.ConsolePos.TopCentered));
-            s_config.Add("ConsoleWidth", config.Bind(CategoryConsoleDraw, "ConsoleWidth", -1, "Positive in pixels. Negative numbers will divide the screen by the number given. Ex. -2 will divide the screen in half, -3 in thirds, etc.."));
-            s_config.Add("ConsoleHeight", config.Bind(CategoryConsoleDraw, "ConsoleHeight", -2, "Positive in pixels. Negative numbers will divide the screen by the number given. Ex. -2 will divide the screen in half, -3 in thirds, etc.."));
-            s_config.Add("ConsoleMaxOutput", config.Bind(CategoryConsoleDraw, "ConsoleMaxOutput", 999, "Maximum number of lines to display in output window."));
-            s_config.Add("ConsoleFontSize", config.Bind(CategoryConsoleDraw, "ConsoleFontSize", 16, "Size of the font within the console window."));
-            s_config.Add("ConsoleDarkenBackground", config.Bind(CategoryConsoleDraw, "ConsoleDarkenBackground", false, "Darken the rest of the screen around the console when visible."));
 
             if (!Settings.Console.ShowConsole)
             {
