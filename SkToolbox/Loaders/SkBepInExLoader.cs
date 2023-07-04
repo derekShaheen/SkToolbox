f8e161ebf601a312a4ffd8fba25f923a20222a51
Derek Shaheen <38998433+derekShaheen@users.noreply.github.com>
2023-07-03 23:18:23 -0500

    Version increment

--- SkToolbox/Loaders/SkBepInExLoader.cs
+++ SkToolbox/Loaders/SkBepInExLoader.cs
@@ -18,7 +18,7 @@ namespace SkToolbox.Loaders
             MODNAME = "SkToolbox",
             AUTHOR = "Skrip",
             GUID = "com." + AUTHOR + "." + MODNAME,
-            VERSION = "2.0.2.3";
+            VERSION = "2.0.2.4";
 
         private static SkBepInExLoader _loader;
         public static SkBepInExLoader Loader { get => _loader; set => _loader = value; }
